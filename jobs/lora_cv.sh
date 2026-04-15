3d9110412af7d525f65421a912d23929d7acac46
s214786 <s214786@student.dtu.dk>
2026-04-15 17:24:21 +0200

    fix: update job queue from gpua40 to gpuh100 and remove unused ranks in lora_cv.sh

--- jobs/lora_cv.sh
+++ jobs/lora_cv.sh
@@ -7,16 +7,18 @@ NOTES="LoRA CV on frozen cnn_standard base. Comparing ranks 1, 2, 4, 8."
 BASE_EXPERIMENT="cnn_standard_cv"
 MODEL="cnn"
 
-for RANK in 1 2 4 8; do
+for RANK in 1 2 4; do
 
 bsub << EOF
 #!/bin/bash
 #BSUB -J lora_cv_r${RANK}
-#BSUB -q gpua40
+#BSUB -q gpuh100
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -n 8
 #BSUB -R "rusage[mem=8GB]"
 #BSUB -W 24:00
+#BSUB -o lora_cv_r${RANK}.out
+#BSUB -e lora_cv_r${RANK}.err
 
 uv run python -m deepsnooze.train \
   model=${MODEL} \
@@ -33,13 +35,15 @@ EOF
 done
 
 NOTES="Bayesian LoRA CV on frozen cnn_standard base. Comparing ranks 1, 2, 4, 8."
-
-for RANK in 1 2 4 8; do
+NOTES="LoRA CV on frozen cnn_standard base. Comparing ranks 1, 2, 4, 8."
+BASE_EXPERIMENT="cnn_standard_cv"
+MODEL="cnn"
+for RANK in 1 2 4; do
 
 bsub << EOF
 #!/bin/bash
 #BSUB -J bayesian_lora_cv_r${RANK}
-#BSUB -q gpua40
+#BSUB -q gpuh100
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -n 8
 #BSUB -R "rusage[mem=8GB]"

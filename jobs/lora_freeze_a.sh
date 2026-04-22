f3b47ba9935edfc0ea3b0346e9cd1b71f79cc9da
s214786 <s214786@student.dtu.dk>
2026-04-22 11:09:26 +0200

    remove notation mistake

--- jobs/lora_freeze_a.sh
+++ jobs/lora_freeze_a.sh
@@ -11,25 +11,25 @@ for RANK in 1 2; do
 
 bsub << EOF
 #!/bin/bash
-#BSUB -J lora_cv_freeze_a_r\${RANK}
+#BSUB -J lora_cv_freeze_a_r${RANK}
 #BSUB -q gpuv100
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -n 8
 #BSUB -R "rusage[mem=8GB]"
 #BSUB -W 24:00
-#BSUB -o lora_cv_freeze_a_r\${RANK}.out
-#BSUB -e lora_cv_freeze_a_r\${RANK}.err
+#BSUB -o lora_cv_freeze_a_r${RANK}.out
+#BSUB -e lora_cv_freeze_a_r${RANK}.err
 
 uv run python -m deepsnooze.train \
-  model=\${MODEL} \
+  model=${MODEL} \
   data=cv \
   training=lora_freeze_a \
-  training.rank=\${RANK} \
+  training.rank=${RANK} \
   training.max_epochs=100 \
-  training.base_experiment=\${BASE_EXPERIMENT} \
-  experiment_name=cnn_standard_lora_freeze_a_r\${RANK}_cv \
-  wandb.group=lora_freeze_a_r\${RANK} \
-  "wandb.notes='\${NOTES}'"
+  training.base_experiment=${BASE_EXPERIMENT} \
+  experiment_name=cnn_standard_lora_freeze_a_r${RANK}_cv \
+  wandb.group=lora_freeze_a_r${RANK} \
+  "wandb.notes='${NOTES}'"
 EOF
 
 done
@@ -41,25 +41,25 @@ for RANK in 1 2; do
 
 bsub << EOF
 #!/bin/bash
-#BSUB -J bayesian_lora_cv_freeze_a_r\${RANK}
+#BSUB -J bayesian_lora_cv_freeze_a_r${RANK}
 #BSUB -q gpuv100
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -n 8
 #BSUB -R "rusage[mem=8GB]"
 #BSUB -W 24:00
-#BSUB -o bayesian_lora_cv_freeze_a_r\${RANK}.out
-#BSUB -e bayesian_lora_cv_freeze_a_r\${RANK}.err
+#BSUB -o bayesian_lora_cv_freeze_a_r${RANK}.out
+#BSUB -e bayesian_lora_cv_freeze_a_r${RANK}.err
 
 uv run python -m deepsnooze.train \
-  model=\${MODEL} \
+  model=${MODEL} \
   data=cv \
   training=bayesian_lora_freeze_a \
-  training.rank=\${RANK} \
+  training.rank=${RANK} \
   training.max_epochs=100 \
-  training.base_experiment=\${BASE_EXPERIMENT} \
-  experiment_name=cnn_standard_bayesian_lora_freeze_a_r\${RANK}_cv \
-  wandb.group=bayesian_lora_freeze_a_r\${RANK} \
-  "wandb.notes='\${NOTES}'"
+  training.base_experiment=${BASE_EXPERIMENT} \
+  experiment_name=cnn_standard_bayesian_lora_freeze_a_r${RANK}_cv \
+  wandb.group=bayesian_lora_freeze_a_r${RANK} \
+  "wandb.notes='${NOTES}'"
 EOF
 
 done
